a0d167c54867bd94159de56613a28ebb7ec19a34
kunalgautam <kunal.gautam@glispamedia.com>
2017-03-14 22:18:27 +0100

    Java Sample Code inheritance

--- javasamples/src/main/java/com/big/data/java/packageA/SubClassAInSamePackage.java
+++ javasamples/src/main/java/com/big/data/java/packageA/SubClassAInSamePackage.java
@@ -21,21 +21,25 @@ public class SubClassAInSamePackage extends SuperClassA {
 
     public static void main(String[] args) {
         // parent refrence pointing to the child class , SuperClassA and SubClassA are in same package
-        SuperClassA superVar = new SubClassAInSamePackage();
+        SuperClassA SubClassAInSamePackage = new SubClassAInSamePackage();
 
-        System.out.println("Parent class method call output for Public is :  " + superVar.getPublicState());
+        System.out.println("Parent class method call output for Public is :  " + SubClassAInSamePackage.getPublicState());
 
-        System.out.println("Parent class method call output for Protected is :  " + superVar.getProtectedState());
+        System.out.println("Parent class method call output for Protected is :  " + SubClassAInSamePackage.getProtectedState());
 
-        System.out.println("Parent class method call output for Private is :  " + superVar.getDefaultState());
+        System.out.println("Parent class method call output for Private is :  " + SubClassAInSamePackage.getDefaultState());
 
         // Child refrence pointing to its own object
-        SubClassAInSamePackage child = (SubClassAInSamePackage) superVar;
+        SubClassAInSamePackage child = (SubClassAInSamePackage) SubClassAInSamePackage;
         System.out.println("Child class method call output for Public is  " + child.getPublicState());
 
         System.out.println("Child class method call output for Protected is  " + child.getProtectedState());
 
         System.out.println("Child class method call output for Private is  " + child.getDefaultState());
+
+        if(SubClassAInSamePackage instanceof SuperClassA){
+
+        }
 
     }
 }
---
+++ javasamples/src/main/java/com/big/data/java/samples/SubClass.java
@@ -0,0 +1,39 @@
+package com.big.data.java.samples;
+
+class superClass {
+    final int b;
+
+    public superClass(int c) {
+        this.b = c;
+        System.out.println("I am inside super class Constructor  :   " + b);
+    }
+}
+
+public class SubClass extends superClass {
+
+    private String state = initializerMethod();
+
+    public SubClass() {
+        super(1);
+        System.out.println("I am inside Constructor");
+        System.out.println("value of state inside Constructor, before is  : " + state);
+        state = "constructor";
+        System.out.println("value of state inside Constructor, after  is  : " + state);
+    }
+
+    private String initializerMethod() {
+        System.out.println("I am inside instance Method initialize ");
+        return "method Initializer";
+    }
+
+    {
+        System.out.println("I am inside instance initialize Block");
+        System.out.println("value of state inside initializer Block is " + state);
+        state = "initializer Block";
+    }
+
+    public static void main(String[] args) {
+        SubClass reference = new SubClass();
+    }
+
+}
