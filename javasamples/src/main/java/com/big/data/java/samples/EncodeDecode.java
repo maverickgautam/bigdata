a634657967c370d0fa89b2477d4bf68822e3f39f
kunalgautam <kunal.gautam@glispamedia.com>
2017-03-23 23:06:09 +0100

    CodeDecode

--- javasamples/src/main/java/com/big/data/java/samples/EncodeDecode.java
+++ javasamples/src/main/java/com/big/data/java/samples/EncodeDecode.java
@@ -1,47 +1,45 @@
-package com.big.data.java.samples;
+    package com.big.data.java.samples;
 
-import java.nio.ByteBuffer;
-import java.util.stream.IntStream;
+    import java.nio.ByteBuffer;
+    import java.util.stream.IntStream;
 
-/**
- * Created by kunalgautam on 23.03.17.
- */
-public class EncodeDecode {
 
-    public static void main(String[] args) {
+    public class EncodeDecode {
 
-        // In Java through character is of two bytes but depending on the encoding settings of the JVM which is UTF-8 it occupy one Byte
-        // "01010101" length is 8 bytes and not 16 bytes
-        String characterRepresentation = "01010101";
+        public static void main(String[] args) {
 
-        // Raw bytes , the schema is no more relvent
-        // Raw bytes are sequence of 1 and 0
-        ByteBuffer rawBytes = ByteBuffer.wrap(characterRepresentation.getBytes());
+            // In Java through character is of two bytes but depending on the encoding settings of the JVM which is UTF-8 it occupy one Byte
+            // "01010101" length is 8 bytes and not 16 bytes
+            String characterRepresentation = "01010101";
 
-        // Char is 2 bytes hence on each call its consuming 2 bytes  hence only 4 iteration required
-        IntStream.range(0, 4)
-                 .forEach(value -> System.out.println("Interprete as CHAR value is " + rawBytes.getChar()));
+            // Raw bytes , the schema is no more relvent
+            // Raw bytes are sequence of 1 and 0
+            ByteBuffer rawBytes = ByteBuffer.wrap(characterRepresentation.getBytes());
 
-        // rewind the ByteBuffer internal pointer
-        rawBytes.rewind();
+            // Char is 2 bytes hence on each call its consuming 2 bytes  hence only 4 iteration required
+            IntStream.range(0, 4)
+                     .forEach(value -> System.out.println("Interprete as CHAR value is " + rawBytes.getChar()));
 
-        IntStream.range(0, 8)
-                 .forEach(value -> System.out.println("Interprete as BYTE value is " + rawBytes.get()));
+            // rewind the ByteBuffer internal pointer
+            rawBytes.rewind();
 
-        // rewind the ByteBuffer internal pointer
-        rawBytes.rewind();
+            IntStream.range(0, 8)
+                     .forEach(value -> System.out.println("Interprete as BYTE value is " + rawBytes.get()));
 
-        // Short is 2 bytes hence on each call its consuming 2 bytes  hence only 4 iteration required
-        IntStream.range(0, 4)
-                 .forEach(value -> System.out.println("Interprete as SHORT value is " + rawBytes.getShort()));
+            // rewind the ByteBuffer internal pointer
+            rawBytes.rewind();
 
-        // rewind the ByteBuffer internal pointer
-        rawBytes.rewind();
+            // Short is 2 bytes hence on each call its consuming 2 bytes  hence only 4 iteration required
+            IntStream.range(0, 4)
+                     .forEach(value -> System.out.println("Interprete as SHORT value is " + rawBytes.getShort()));
 
-        // rawBytes is of length 4 hence we can interprete it as Int
-        // In java Int is 4 bytes
-        System.out.println("Interprete as INT value1 is " + rawBytes.getInt());
-        System.out.println("Interprete as INT value2 is " + rawBytes.getInt());
+            // rewind the ByteBuffer internal pointer
+            rawBytes.rewind();
 
+            // rawBytes is of length 4 hence we can interprete it as Int
+            // In java Int is 4 bytes
+            IntStream.range(0, 2)
+                     .forEach(value -> System.out.println("Interprete as INT value is " + rawBytes.getInt()));
+
+        }
     }
-}
